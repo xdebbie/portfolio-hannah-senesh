c866065180c2fa09a10bb0373409863e91facdff
xdebbie <deh@live.in>
2020-07-03 16:49:02 +0200

    Update configs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import "../styles/styles.scss"
 
-import Scrollbar from "smooth-scrollbar"
+import scrollTo from 'gatsby-plugin-smoothscroll';
 
 // Import components
 import Header from "../components/header"
