1af2451ed19c771715041ab488ee5f73bc58c84d
xdebbie <deh@live.in>
2020-07-03 11:50:05 +0200

    Update files

---
+++ src/components/banner.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="container">
+                <div className="row">
+                    <div className="main-text">
+                        Oladimeji Odunsi    
+                    </div>
+                    <div className="main-image">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
