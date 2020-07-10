7a28fa5eb1fb8ebad6d9e8567f85103131430a95
xdebbie <deh@live.in>
2020-07-10 17:11:18 +0200

    Update about section

---
+++ src/components/workseries.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Workseries = () => {
+  return (
+    <div className="gallery">
+      <h1>Images from Unsplash...</h1>
+      <p>
+        Now this is the Law of the Jungle, as old and true as the sky, for as
+        long as you keep scrolling, you shall find more doggo images.{" "}
+      </p>
+    </div>
+  )
+}
+
+export default Workseries
+
+
---
+++ src/pages/work.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "../styles/styles.scss"
+
+// Import components
+import Header from "../components/header"
+import Workseries from "../components/workseries"
+
+const WorkPage = () => (
+  <div>
+    <Header />
+    <Workseries />
+  </div>
+)
+
+export default WorkPage
