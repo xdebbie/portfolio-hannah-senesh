255aab057e7d04fcb211c5465df1e5374a1fde16
xdebbie <deh@live.in>
2020-07-14 20:25:01 +0200

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv/config")
+require("./src/components/map")
 module.exports = {
   siteMetadata: {
     title: `Gatsby Portfolio`,
@@ -37,13 +39,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `@ccalamos/gatsby-source-googlemaps-static`,
-      options: {
-        key: `AIzaSyDQNs-42abkWnIejk89TSDDdZ6D8D_mEcg`,
-        center: `Stockholm, Sweden`,
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
